f72902c8e62b125bba5f1da317f363ecdd5caca8
Sorocean Sergiu <soroceansergiu10@gmail.com>
2024-12-02 23:41:04 +0200

    Added login dto

---
+++ src/Personal.Dara.Hub.Server.Models/Data transfer object/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Personal.Dara.Hub.Server.Models.Data_transfer_object
+{
+    public class LoginDTO
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required bool RemeberUser { get; set; }
+    }
+}
