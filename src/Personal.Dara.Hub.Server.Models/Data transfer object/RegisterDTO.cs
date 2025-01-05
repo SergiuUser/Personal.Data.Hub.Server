c542232e82b7313b06c2d7ea3b5260d5bdc8816a
Sorocean Sergiu <soroceansergiu10@gmail.com>
2025-01-05 22:06:58 +0200

    Added registerDTO

---
+++ src/Personal.Dara.Hub.Server.Models/Data transfer object/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Personal.Dara.Hub.Server.Models.Data_transfer_object
+{
+    public class RegisterDTO
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
+        public required string Email { get; set; }
+    }
+}
