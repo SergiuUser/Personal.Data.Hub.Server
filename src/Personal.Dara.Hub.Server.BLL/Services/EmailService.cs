264ae18ce95eb25cb429fc0e221fca1080886044
Sorocean Sergiu <soroceansergiu10@gmail.com>
2025-01-13 22:02:02 +0200

    Implemented interface

--- src/Personal.Dara.Hub.Server.BLL/Services/EmailService.cs
+++ src/Personal.Dara.Hub.Server.BLL/Services/EmailService.cs
@@ -1,6 +1,12 @@
-﻿namespace Personal.Dara.Hub.Server.BLL.Services
+﻿using Personal.Dara.Hub.Server.BLL.Services.Interfaces;
+
+namespace Personal.Dara.Hub.Server.BLL.Services
 {
-    public class EmailService
+    public class EmailService : IEmailService
     {
+        public Task<bool> SendEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/Personal.Dara.Hub.Server.BLL/Services/Interfaces/IEmailService.cs
+++ src/Personal.Dara.Hub.Server.BLL/Services/Interfaces/IEmailService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Personal.Dara.Hub.Server.BLL.Services.Interfaces
+﻿namespace Personal.Dara.Hub.Server.BLL.Services.Interfaces
 {
     public interface IEmailService
     {
